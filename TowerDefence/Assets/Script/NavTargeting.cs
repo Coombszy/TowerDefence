11c15654a7d1b287797752d83d15ac935dd5768b
Coombszy <LCCoombs@hotmail.co.uk>
2019-01-07 14:17:53 +0000

    Basic scene with some test enemy navigation. This acts as a good start point for the project

---
+++ TowerDefence/Assets/Script/NavTargeting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class NavTargeting : MonoBehaviour {
+	public GameObject Target;//where it is traveling too
+
+	private NavMeshAgent NavComponent;
+
+	// Use this for initialization
+	void Start () {
+		NavComponent = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Target)//is target there
+		{
+			NavComponent.SetDestination(Target.transform.position);
+		}
+	}
+}
