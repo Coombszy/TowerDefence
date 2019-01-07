7c6c96e1dccb2818594cb21952dd694485775ba5
Coombszy <LCCoombs@hotmail.co.uk>
2019-01-07 15:31:45 +0000

    Added more enemies and configured nav mesh to allow travel up slopes. Also added collision ignore between each other but will avoid too much contact. also tested idea of tunnel into user base/attack zone

---
+++ TowerDefence/Assets/Script/IgnoreTag.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreTag : MonoBehaviour {
+	public string TagToIgnore;
+	void OnCollisionEnter(Collision collision)
+    {
+         if (collision.gameObject.tag == TagToIgnore)
+        {
+					
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), this.GetComponent<Collider>());
+        }
+	}
+}
