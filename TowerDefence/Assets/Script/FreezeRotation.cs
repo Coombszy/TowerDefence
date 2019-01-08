166bd8bdb787fea8c69a6d1cf1c4488f8e08611a
Coombszy <LCCoombs@hotmail.co.uk>
2019-01-08 10:13:13 +0000

    Created prefabs of enemies and base. Also created a summoner script that can be attached to a game object to spawn enemies as well as wipe the spawned wave

---
+++ TowerDefence/Assets/Script/FreezeRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeRotation : MonoBehaviour {
+
+ 	Rigidbody ObjectsRigidbody;
+	void Start () {
+		ObjectsRigidbody = GetComponent<Rigidbody>();
+    ObjectsRigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+	}
+	
+}
---
+++ TowerDefence/Assets/Script/Summoner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Summoner : MonoBehaviour {
+	public GameObject EnemyPrefab;
+	public int Quantity = 5;
+	public float Offset = 2;
+	public bool SpawnOnStart = false;
+
+	private List<GameObject> SpawnedEnemies = new List<GameObject>();
+	
+	void Start()
+	{
+		if(SpawnOnStart)
+		{
+			SpawnWave();
+		}
+	}
+
+	public void SpawnWave()
+	{
+		Vector3 CurrentLocation = this.transform.position;
+		for(int i = 0; i < Quantity; i++)
+		{
+			SpawnedEnemies.Add(Instantiate(EnemyPrefab, CurrentLocation + new Vector3(Random.Range(-Offset,Offset),0,Random.Range(-Offset,Offset)), Quaternion.identity));
+		}
+	}
+
+	public void WipeWave()
+	{
+		foreach(GameObject Enemy in SpawnedEnemies)
+		{
+			Destroy(Enemy,0);
+		}
+	}
+}
