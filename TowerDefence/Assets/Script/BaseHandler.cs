d6e214511ce28b6b75381709ee66c23188e92b9b
Coombszy <LCCoombs@hotmail.co.uk>
2019-01-08 11:27:12 +0000

    Added health mechanic and enemy destruction on base collision

---
+++ TowerDefence/Assets/Script/BaseHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseHandler : MonoBehaviour {
+
+	public int Health = 1000;
+	public string EnemyTag = "Enemy";
+	public GameObject HealthTextBox;
+
+	void Start()
+	{
+		HealthTextBox.GetComponent<Text>().text = "HP:"+Health;
+	}
+	void OnCollisionEnter(Collision collision)
+    {
+         if (collision.gameObject.tag == EnemyTag)
+        {
+            Health -= 1;
+						HealthTextBox.GetComponent<Text>().text = "HP:"+Health;
+						Destroy(collision.gameObject);
+        }
+	}
+}
---
+++ TowerDefence/Assets/Script/HealthManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+	public int Health = 100;
+	
+	public void DealDamage(int Damage)
+	{
+		Health =- Damage;
+		if(Health <1)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
